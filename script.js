e697db8cc4fcfdb9a67ff55f4e3b567eff76aa1f
braeden-baldwin <baldwin.braeden.a@gmail.com>
2025-03-28 14:44:36 -0400

    Initial push

---
+++ script.js
@@ -0,0 +1,7 @@
+//Not much here yet
+
+function calculateCost(){
+    //Need cost of vehicle, term, APR, and down payment
+    //c = principal cost, n=term(number of months), r = apr/12 d=down payment
+    //payment = P-down * (r * (1 + r)^n) / ((1 + r)^n - 1)
+}
