baa9d965578392c40502aeb89820a69f101add09
braeden-baldwin <baldwin.braeden.a@gmail.com>
2025-04-11 11:55:32 -0400

    functional calculator shown in framework

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 //Not much here yet
+window.addEventListener("DOMContentLoaded", domLoaded);
 
-function calculateCost(){
+let termList = [36,48,60,72];
+
+function domLoaded(){
+    document.getElementById("calculateButton").addEventListener("click",updateCost)
+}
+
+function calculateCost(cost, down, APR, term){
     //Need cost of vehicle, term, APR, and down payment
-    //c = principal cost, n=term(number of months), r = apr/12 d=down payment
-    //payment = P-down * (r * (1 + r)^n) / ((1 + r)^n - 1)
+    r = APR/12;
+    p = (cost-down)
+    //p = principal cost, n=term(number of months), r = apr/12 d=down payment
+    payment = (p * r) / (1 - (1 + r)^-term)
+    return payment;
+}
+
+function updateCost(){
+    let html = "";
+    let cost = (document.getElementById("cost").value);
+    let down = (document.getElementById("down").value);
+    let APR = (document.getElementById("APR").value);
+
+    for (let index = 0; index <4; index++){
+        let payment = calculateCost(cost,down,APR, termList[index]);
+        let addedhtml = (`<p>The monthly payment for a ${termList[index]} term is: ${payment.toFixed(2)}</p>`)
+        html+=addedhtml;
+    }
+
+    document.getElementById("payments").innerHTML=html;
+
 }
